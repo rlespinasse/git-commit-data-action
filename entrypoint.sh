a5bd5526454653fb324225e7309d8e289c495d87
rlespinasse <romain.lespinasse@gmail.com>
2020-10-07 22:37:57 +0200

    fix: remove set-env due to CVE fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,11 @@ git_log_format() {
   git log -1 --pretty=format:"$pattern" "$reference"
 }
 
-echo ::set-env name=GIT_COMMIT_AUTHOR::"$(git_log_format "$AUTHOR_FORMAT" "HEAD")"
-echo ::set-env name=GIT_COMMIT_AUTHOR_NAME::"$(git_log_format "$AUTHOR_NAME_FORMAT" "HEAD")"
-echo ::set-env name=GIT_COMMIT_AUTHOR_EMAIL::"$(git_log_format "$AUTHOR_EMAIL_FORMAT" "HEAD")"
-
-echo ::set-env name=GIT_COMMIT_COMMITTER::"$(git_log_format "$COMMITTER_FORMAT" "HEAD")"
-echo ::set-env name=GIT_COMMIT_COMMITTER_NAME::"$(git_log_format "$COMMITTER_NAME_FORMAT" "HEAD")"
-echo ::set-env name=GIT_COMMIT_COMMITTER_EMAIL::"$(git_log_format "$COMMITTER_EMAIL_FORMAT" "HEAD")"
+{
+  echo "GIT_COMMIT_AUTHOR=$(git_log_format "$AUTHOR_FORMAT" "HEAD")"
+  echo "GIT_COMMIT_AUTHOR_NAME=$(git_log_format "$AUTHOR_NAME_FORMAT" "HEAD")"
+  echo "GIT_COMMIT_AUTHOR_EMAIL=$(git_log_format "$AUTHOR_EMAIL_FORMAT" "HEAD")"
+  echo "GIT_COMMIT_COMMITTER=$(git_log_format "$COMMITTER_FORMAT" "HEAD")"
+  echo "GIT_COMMIT_COMMITTER_NAME=$(git_log_format "$COMMITTER_NAME_FORMAT" "HEAD")"
+  echo "GIT_COMMIT_COMMITTER_EMAIL=$(git_log_format "$COMMITTER_EMAIL_FORMAT" "HEAD")"
+} >>"$GITHUB_ENV"
